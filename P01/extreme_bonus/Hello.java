946487784528c7818aa3a69b4d17160fcf1c0815
Juan Guajardo Gutierrez <guajardo5423@gmail.com>
2024-08-25 13:32:31 -0500

    Fixed syntax error in extreme bonus code

--- P01/extreme_bonus/Hello.java
+++ P01/extreme_bonus/Hello.java
@@ -3,6 +3,6 @@ public class Hello
 	public static void main(String args[])
 	{
 		String name = System.getProperty("user.name");
-		println("hello, " + name + "!");
+		System.out.println("hello, " + name + "!");
 	}
 }
