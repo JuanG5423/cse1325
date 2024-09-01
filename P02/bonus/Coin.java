6ea4a29898993e655525538f1de621591a4035e6
Juan Guajardo Gutierrez <guajardo5423@gmail.com>
2024-08-31 22:40:49 -0500

    Wrote Coin class for bonus code

--- P02/bonus/Coin.java
+++ P02/bonus/Coin.java
@@ -11,27 +11,16 @@ public class Coin
 
 	public double getValue()
 	{
-		double value = 0;
-		switch(denomination)
-		{
-			case PENNY:
-				value = 0.01;
-				break;
-			case NICKEL:
-				value = 0.05;
-				break;
-			case DIME:
-				value = 0.1;
-				break;
-			case QUARTER:
-				value = 0.25;
-				break;
-		}
-		return value;
+		return denomination.getValue();
 	}
 
 	public int getYear()
 	{
 		return year;
 	}
+
+	@Override
+	public String toString()
+	{
+		return year + " " + denomination;
 }
