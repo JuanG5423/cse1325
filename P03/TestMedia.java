3177c0ce36c87564dfaa35a5235b035b012148aa
Juan Guajardo Gutierrez <guajardo5423@gmail.com>
2024-09-09 15:29:47 -0500

    Fixed error in TestMedia regression test

--- P03/TestMedia.java
+++ P03/TestMedia.java
@@ -4,7 +4,7 @@ public class TestMedia
 	{
 		//TEST VECTOR: Verify that the toString() returns the title followed by the URL in parentheses
 		Media media = new Media("The Little Shop of Horrors", "https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0");
-		if (!(media.toString().equals("The Little Shop of Horrors (https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0)")));
+		if (!(media.toString().equals("The Little Shop of Horrors (https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0)")))
 		{
 			System.err.println("FAIL: expected output is \"The Little Shop of Horrors (https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0)\" but returned " + media.toString());
 			System.exit(69);
