67c3633b99b4609c7f15c4ba9986555813865309
Juan Guajardo Gutierrez <guajardo5423@gmail.com>
2024-09-09 16:03:52 -0500

    Wrote TestStudent second test vector

--- P03/TestStudent.java
+++ P03/TestStudent.java
@@ -7,9 +7,16 @@ public class TestStudent
 		//TEST VECTOR #1: Student.toString()
 		if (!(student.toString().equals("Juan Guajardo Gutierrez (1002128662, jgg8662@mavs.uta.edu, Account #1)")))
 		{
-			System.err.println("Expected output is Juan Guajardo Gutierrez (1002128662, jgg8662@mavs.uta.edu, Account #1) but received " + student.toString());
+			System.err.println("FAIL: Expected output is Juan Guajardo Gutierrez (1002128662, jgg8662@mavs.uta.edu, Account #1) but received " + student.toString());
 			System.exit(69);
 		}
+
+		//TEST VECTOR #2: Student.requestMedia(media)
+		if (!(student.requestMedia(new Media("The Little Shop of Horrors", "https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0")).equals("Playing The Little Shop of Horrors (https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0)")))
+		{
+			System.err.println("FAIL: Expected output is Playing The Little Shop of Horrors (https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0) but returned " + student.requestMedia(new Media("The Little Shop of Horrors", "https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0")));
+			System.exit(69);
+		}	
 	}
 }
 
