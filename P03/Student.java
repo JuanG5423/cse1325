1e3b8a25ce335266b4f3f5e7cc21d64f7824640a
Juan Guajardo Gutierrez <guajardo5423@gmail.com>
2024-09-09 14:34:07 -0500

    Created P03 with build.xml and started creating Student class

---
+++ P03/Student.java
@@ -0,0 +1,25 @@
+public class Student
+{
+	private String name;
+	private int id;
+	private String email;
+	private Account account;
+
+	public Student(String name, int id, String email)
+	{
+		this.name = name;
+		this.id = id;
+		this.email = email;
+	}
+
+	public String requestMedia(Media media)
+	{
+		continue;
+	}
+
+	@Override
+	public String toString()
+	{
+		return name;
+	}
+}
