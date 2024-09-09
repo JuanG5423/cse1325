c5d6490829f88eca6e070ca96eb3ac9291902163
Juan Guajardo Gutierrez <guajardo5423@gmail.com>
2024-09-09 15:11:59 -0500

    Wrote TestAccount regression tests

--- P03/TestMedia.java
+++ P03/TestMedia.java
@@ -3,7 +3,7 @@ public class TestMedia
 	public static void main(String[] args)
 	{
 		//TEST VECTOR: Verify that the toString() returns the title followed by the URL in parentheses
-		media = new Media("The Little Shop of Horrors", "https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0");
+		Media media = new Media("The Little Shop of Horrors", "https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0");
 		if (!(media.toString().equals("The Little Shop of Horrors (https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0)")));
 		{
 			System.err.println("FAIL: expected output is \"The Little Shop of Horrors (https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0)\" but returned " + media.toString());
