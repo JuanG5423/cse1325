a0bc1120119555c674b6ed73e79d563c0fa09e23
Juan Guajardo Gutierrez <guajardo5423@gmail.com>
2024-09-09 20:00:05 -0500

    Added functionality to verify URLs in Media class

--- P03/Media.java
+++ P03/Media.java
@@ -1,3 +1,10 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.time.Duration;
+
 public class Media
 {
 	private String title;
@@ -5,8 +12,19 @@ public class Media
 
 	public Media(String title, String url)
 	{
-		this.title = title;
-		this.url = url;
+		
+		try 
+		{
+		    HttpClient client = HttpClient.newHttpClient();
+		    HttpRequest request = HttpRequest.newBuilder().uri(new URI(url)).timeout(Duration.ofSeconds(15)).build();
+		    HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+		    this.title = title;
+		    this.url = url;
+		}
+	       	catch (Exception e) 
+		{
+		    throw new IllegalArgumentException("Invalid URL");
+		}
 	}
 
 	@Override
--- P03/TestStudent.java
+++ P03/TestStudent.java
@@ -3,19 +3,20 @@ public class TestStudent
 	public static void main(String[] args)
 	{
 		Student student = new Student("Juan Guajardo Gutierrez", 1002128662, "jgg8662@mavs.uta.edu");
+		int errors = 0;
 
 		//TEST VECTOR #1: Student.toString()
 		if (!(student.toString().equals("Juan Guajardo Gutierrez (1002128662, jgg8662@mavs.uta.edu, Account #1)")))
 		{
 			System.err.println("FAIL: Expected output is Juan Guajardo Gutierrez (1002128662, jgg8662@mavs.uta.edu, Account #1) but received " + student.toString());
-			System.exit(69);
+			errors++;
 		}
 
 		//TEST VECTOR #2: Student.requestMedia(media)
 		if (!(student.requestMedia(new Media("The Little Shop of Horrors", "https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0")).equals("Playing The Little Shop of Horrors (https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0)")))
 		{
 			System.err.println("FAIL: Expected output is Playing The Little Shop of Horrors (https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0) but returned " + student.requestMedia(new Media("The Little Shop of Horrors", "https://publicdomainmovie.net/movie/the-little-shop-of-horrors-0")));
-			System.exit(69);
+			errors++;
 		}	
 
 		//TEST VECTOR #3: Non-UTA email address
@@ -23,6 +24,7 @@ public class TestStudent
 		{
 			Student student2 = new Student("Invalid Email", 7777777, "bidibidibombom@utd.edu");
 			System.err.println("FAIL: Non-UTA email address (bidibidibombom@utd.edu) but no exception thrown");
+			errors++;
 		}
 		catch (IllegalArgumentException e)
 		{
@@ -30,7 +32,10 @@ public class TestStudent
 		catch (Exception e)
 		{
 			System.err.println("FAIL: Student threw wrong exception on Non-UTA email address");
+			errors++;
 		}
+
+		System.exit(errors);
 	}
 }
 
