ee27d2535c8822b3cd848748f1b719b9a3874e1b
Juan Guajardo Gutierrez <guajardo5423@gmail.com>
2024-09-15 21:43:56 -0500

    Finished writing Moes class

--- P04/moes/Moes.java
+++ P04/moes/Moes.java
@@ -2,6 +2,9 @@ package moes;
 import java.util.ArrayList;
 import product.Media;
 import customer.Student;
+import customer.Account;
+import customer.Unlimited;
+import customer.Alacarte;
 
 public class Moes
 {
@@ -43,5 +46,48 @@ public class Moes
 		return sb.toString();
 	}
 
+	public int getPoints(int studentIndex)
+	{
+		Account account = customers.get(studentIndex).getAccount();
+
+		if (account instanceof Alacarte)
+		{
+			Alacarte alacarteAccount = (Alacarte) account;
+			return alacarteAccount.getPointsRemaining();
+		}
+		else if (account instanceof Unlimited)
+		{
+			return Integer.MAX_VALUE;
+		}
+		else
+		{
+			throw new UnsupportedOperationException("Unknown subclass of Account");
+		}
+	}
+
+	public String buyPoints(int studentIndex, int points)
+	{
+		Account account = customers.get(studentIndex).getAccount();
+
+		if (account instanceof Alacarte)
+		{
+			Alacarte alacarteAccount = (Alacarte) account;
+			alacarteAccount.buyPoints(points);
+			return "You now have " + alacarteAccount.getPointsRemaining() + " points";
+		}
+		else if (account instanceof Unlimited)
+		{
+			return "You have an unlimited account and don't need additional points";
+		}
+		else
+		{
+			throw new UnsupportedOperationException("Unknown subclass of Account");
+		}
+	}
+
+	public String playMedia(int studentIndex, int mediaIndex)
+	{
+		return customers.get(studentIndex).requestMedia(library.get(mediaIndex));
+	}
 }
 
