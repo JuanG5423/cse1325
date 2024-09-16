0b8731d30eba50db90d8eee30fdb117d44abf197
Juan Guajardo Gutierrez <guajardo5423@gmail.com>
2024-09-16 13:27:41 -0500

    Added second test vector to TestMoes

--- P04/test/TestMoes.java
+++ P04/test/TestMoes.java
@@ -20,6 +20,17 @@ public class TestMoes
 			System.err.println("Expected output of \n0) Bidi Bidi Bom Bom (https://www.last.fm/music/Selena/_/Bidi+Bidi+Bom+Bom, 5000 points)\n1) Telepatia (https://www.last.fm/music/Kali+Uchis/_/telepat%C3%ADa, 2147483647 points)\n but received \n" + moes.getMediaList());
 			errors++;
 		}
+
+		//TEST VECTOR #2: getStudentList
+		moes.addStudent(new Student("Unlimited Limited", 7777, "unlimited@mavs.uta.edu", true));
+		moes.addStudent(new Student("Limited Unlimited", 9999, "notunlimited@uta.edu", false));
+		if (!(moes.getStudentList().equals("0) Unlimited Limited (7777, unlimited@mavs.uta.edu, Account #1)\n1) Limited Unlimited (9999, notunlimited@uta.edu, Account #2)\n")))
+		{
+			System.err.println("Expected output of \n0) Unlimited Limited (7777, unlimited@mavs.uta.edu, Account #1)\n1) Limited Unlimited (9999, notunlimited@uta.edu, Account #2)\n but received \n" + moes.getStudentList());
+			errors++;
+		}
+
+		System.exit(errors);
 	}
 }
 		
