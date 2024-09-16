50ab6ee345761e2a670c27ab0f76a2360bf44c18
Juan Guajardo Gutierrez <guajardo5423@gmail.com>
2024-09-15 19:20:31 -0500

    Created Alacarte subclass

---
+++ P04/customer/Alacarte.java
@@ -0,0 +1,32 @@
+package customer;
+import product.Media;
+
+public class Alacarte extends Account
+{
+	private int pointsRemaining;
+
+	public void buyPoints(int points)
+	{
+		pointsRemaining += points;
+	}
+
+	public int getPointsRemaining()
+	{
+		return pointsRemaining;
+	}
+
+	@Override
+	public String play(Media media)
+	{
+		if (pointsRemaining >= media.getPoints())
+		{
+			pointsRemaining -= media.getPoints();
+			return "Playing " + media.toString();
+		}
+		else
+		{
+			return "Buy more points: Requires " + media.getPoints() + " points, you have " + pointsRemaining;
+		}
+	}
+}
+
