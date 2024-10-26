03001d16bc2b57abfd4b6898822b977a2e87ef03
JuanG5423 <guajardo5423@gmail.com>
2024-10-26 13:09:48 -0500

    Started writing nums.cpp

---
+++ P08/bonus/nums.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+
+int main(int argc, char **argv)
+{
+	std::vector<std::string> numbers;
+	std::vector<std::string> *words = new std::vector<std::string>;
+}	
+
