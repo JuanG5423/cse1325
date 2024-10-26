931fa227dd54af4b1a94c7c579a8978909f03a35
JuanG5423 <guajardo5423@gmail.com>
2024-10-26 18:17:01 -0500

    Wrote code to fill vectors

--- P08/bonus/nums.cpp
+++ P08/bonus/nums.cpp
@@ -5,5 +5,21 @@ int main(int argc, char **argv)
 {
 	std::vector<std::string> numbers;
 	std::vector<std::string> *words = new std::vector<std::string>;
+	
+	for (int i = 1; i < argc; i++)
+	{
+		std::string str(argv[i]);
+		if (isdigit(argv[i][0]))
+		{
+			numbers.push_back(argv[i]);
+		}
+		else
+		{
+			(*words).push_back(argv[i]);
+		}
+	}
+
+	std::cout << "Numbers: " << std::endl;
+	
 }	
 
