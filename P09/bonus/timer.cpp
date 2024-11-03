d2d88d81a44b9170135c4c75dd69f0f105f04e17
JuanG5423 <guajardo5423@gmail.com>
2024-11-03 12:49:02 -0600

    Wrote timer.cpp

---
+++ P09/bonus/timer.cpp
@@ -0,0 +1,29 @@
+#include "timer.h"
+#include <iostream>
+
+Timer::Timer(int hours, int minutes, int seconds) : Clock{hours, minutes, seconds}
+{
+}
+
+Timer::~Timer()
+{
+}
+
+void Timer::tic()
+{
+	_seconds--;
+	if (_seconds < 0)
+	{
+		_seconds += 60;
+		_minutes--;
+	}
+	if (_minutes < 0)
+	{
+		_minutes += 60;
+		_hours--;
+	}
+	if (!_hours && !_minutes && _seconds)
+	{
+		throw std::runtime_error{"Timer expired"};
+	}
+}
