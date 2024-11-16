d812e932fb5106ebd64907e39f329ab38537231d
JuanG5423 <guajardo5423@gmail.com>
2024-11-16 14:48:05 -0600

    Created P11 directory and date.h

---
+++ P10/bonus/bank.cpp
@@ -0,0 +1,39 @@
+#include "purse.h"
+#include <map>
+
+int main()
+{
+	std::map<std::string, Purse> vault;
+	int accounts;
+	std::cout << "Welcome to Ye Olde Bank of Merry England" << std::endl << std::endl << std::endl << "How many accounts? ";
+	std::cin >> accounts;
+	for (int i = 0; i < accounts; ++i)
+	{
+		std::cout << "Name account " << i << ": ";
+		std::string name;
+		std::cin.ignore();
+		std::getline(std::cin, name);
+		int pence;
+		int shillings;
+		int pounds;
+		std::cout << "Enter your initial deposit (pounds shillings pence): ";
+		std::cin >> pounds;
+		std::cin >> shillings;
+		std::cin >> pence;
+		Purse purse{pounds, shillings, pence};
+		vault[name] = purse;
+		std::cout << "Account " << name << " created with " << purse << std::endl << std::endl;
+	}
+
+	std::cout << std::endl << std::endl << std::endl << "Account List" << std::endl << "============" << std::endl << std::endl;
+	Purse total{};
+	
+	for (const auto& pair : vault)
+	{
+		std::cout << pair.first << " with " << pair.second << std::endl;
+		total += pair.second;
+	}
+	std::cout << std::endl << "Total in bank is " << total << std::endl;
+
+
+}
---
+++ P10/bonus/purse.cpp
@@ -0,0 +1,89 @@
+#include "purse.h"
+
+Purse::Purse(int pounds, int shillings, int pence) : _pounds{pounds}, _shillings{shillings}, _pence{pence}
+{
+	rationalize();
+}
+
+std::ostream& operator<<(std::ostream& ost, const Purse& purse)
+{
+	return ost << "£" << purse._pounds << " " << purse._shillings << "s" << purse._pence << "d";
+}
+
+std::istream& operator>>(std::istream& ist, const Purse& purse)
+{
+	ist >> purse._pounds >> purse._shillings >> purse._pence;
+	return ist;
+}
+
+Purse& Purse::operator++()
+{
+	++_pence;
+	rationalize();
+	return *this;
+}
+
+Purse Purse::operator++(int)
+{
+	Purse purse{*this};
+	++*this;
+	return purse;
+}
+
+Purse Purse::operator+(const Purse& purse)
+{
+	return Purse{_pounds + purse._pounds, _shillings + purse._shillings, _pence + purse._pence};
+}
+
+Purse Purse::operator-(const Purse& purse)
+{
+	return Purse{_pounds - purse._pounds, _shillings - purse._shillings, _pence - purse._pence};
+}
+
+Purse& Purse::operator+=(const Purse& purse)
+{
+	_pounds += purse._pounds;
+	_shillings += purse._shillings;
+	_pence += purse._pence;
+	rationalize();
+	return *this;
+}
+
+Purse& Purse::operator-=(const Purse& purse)
+{
+	_pounds -= purse._pounds;
+	_shillings -= purse._shillings;
+	_pence -= purse._pence;
+	rationalize();
+	return *this;
+}
+
+void Purse::rationalize()
+{
+	while (_pence >= 12 || _pence < 0)
+	{
+		if (_pence >= 12)
+		{
+			_pence -= 12;
+			_shillings++;
+		}
+		if (_pence < 0)
+		{
+			_pence += 12;
+			_shillings--;
+		}
+	}
+	while (_shillings >= 20 || _shillings < 0)
+	{	
+		if (_shillings >= 20)
+		{
+			_shillings -= 20;
+			_pounds++;
+		}
+		if (_shillings < 0)
+		{
+			_shillings += 20;
+			_pounds--;
+		}
+	}	
+}
