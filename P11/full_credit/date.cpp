6fcd3e546e20a5ba195eba718b01a4f21faa0de2
JuanG5423 <guajardo5423@gmail.com>
2024-11-16 15:52:37 -0600

    Added Makefile and fixed logic error in date.cpp

--- P11/full_credit/date.cpp
+++ P11/full_credit/date.cpp
@@ -7,6 +7,6 @@ Date::Date(int year, int month, int day) : _year{year}, _month{month}, _day{day}
 
 std::ostream& operator<<(std::ostream& ost, const Date& date)
 {
-	return ost << std::setw(4) << _year << "/" << std::setw(2) << std::setfill('0') << _month << "/" << std::setw(2) << std::setfill('0') << _day;
+	return ost << std::setw(4) << date._year << "/" << std::setw(2) << std::setfill('0') << date._month << "/" << std::setw(2) << std::setfill('0') << date._day;
 }
 
