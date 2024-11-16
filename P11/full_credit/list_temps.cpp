f633193c68aae8e7f1823e1f5d3990e17cf7c428
JuanG5423 <guajardo5423@gmail.com>
2024-11-16 15:48:01 -0600

    Added data processing

--- P11/full_credit/list_temps.cpp
+++ P11/full_credit/list_temps.cpp
@@ -1,6 +1,7 @@
 #include "date.h"
 #include <map>
 #include <fstream>
+#include <sstream>
 
 int main(int argc, char **argv)
 {
@@ -19,5 +20,35 @@ int main(int argc, char **argv)
 			exit(-1);
 		}
 		std::map<Date, Temp> temps;
+		std::string line;
+		while (std::getline(ifs, line))
+		{
+			std::istringstream iss{line};
+			std::string value;
+
+			for (int i = 0; i < 4; i++)
+			{
+				std::getline(iss, value, ',');
+			}
+
+			int year;
+			int month;
+			int day;
+			
+			std::getline(iss, value, ',');
+			month = std::stoi(value);
+
+			std::getline(iss, value, ',');
+			day = std::stoi(value);
+
+			std::getline(iss, value, ',');
+			year = std::stoi(value);
+
+			std::getline(iss, value, ',');
+			Temp temperature = std::stod(value);
+
+			Date date{year, month, day};
+			temps[date] = temperature;
+		}
 	}
 }
