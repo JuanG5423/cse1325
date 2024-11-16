63e61204cbb71045b3d2095127a0d31a3eeddc88
JuanG5423 <guajardo5423@gmail.com>
2024-11-16 15:07:46 -0600

    Created lists_temps.pp

---
+++ P11/full_credit/list_temps.cpp
@@ -0,0 +1,14 @@
+#include "date.h"
+
+int main(int argc, char **argv)
+{
+	typedef double Temp;
+	if (argc <= 1)
+	{
+		std::cout << "usage: <program name> <data file>." << std::endl;
+	}
+	else
+	{
+		std::string filename{argv[1]};
+	}
+}
